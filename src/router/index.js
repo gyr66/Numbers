d38d09e3c0405999a6f093c5e24bae0ba7f90566
gyr <157679566@qq.com>
2021-07-28 21:14:12 +0800

    增加全局导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -14,16 +14,27 @@ const routes = [
   },
   {
     path: "/scoreList",
-    component: ScoreList
+    component: ScoreList,
+    meta: {
+      title: "历史记录"
+    }
   },
   {
     path: "/challengeView",
-    component: ChallengeView
+    component: ChallengeView,
+    meta: {
+      title: "挑战"
+    }
   }
 ];
 
 const router = new VueRouter({
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.matched[0].meta.title;
+  next();
 });
 
 export default router;
